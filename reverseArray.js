f1a8b3f8255707227a59d1c2a2271a29197a435c
lukew244 <luke.webster86@gmail.com>
2016-10-07 17:51:53 +0100

    nth of list

--- reverseArray.js
+++ reverseArray.js
@@ -15,11 +15,17 @@ function reverseInPlace(array) {
 
 function arrayToList (array) {
   var list = {};
-  for (var i=(array.length - 1); i >= 0; i--) {
-      list = { value: i, rest: list};
+  if (array.length > 1) {
+    list.value = array.shift();
+    list.rest = arrayToList(array);
   }
+  else {
+    list.value = array.shift();
+    list.rest = null;
+    }
   return list;
 }
+
 
 
 function listToArray(list) {
@@ -30,9 +36,27 @@ function listToArray(list) {
   return array;
 }
 
+function prepend(list, element) {
+  var array = listToArray(list);
+  array.unshift(element);
+  var newList = arrayToList(array);
+  return newList;
+}
+
+function nth(list, number) {
+  if(!list)
+    return undefined;
+  else if (number === 0)
+    return list.value;
+  else
+    return nth(list.rest, number-1);
+
+}
 
 
-console.log(arrayToList([1,2,3]));
-console.log(listToArray(arrayToList([1,2,3])));
 console.log(reverseArray([1,2,3,4,5]));
 console.log(reverseInPlace([1,2,3,4,5]));
+console.log(arrayToList([1,2,3]));
+console.log(listToArray(arrayToList([1,2,3])));
+console.log(prepend(arrayToList([1,2,3]), 8));
+console.log(nth(arrayToList([1,2,3]), 1));
